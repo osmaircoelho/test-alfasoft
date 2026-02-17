5a2ae9a1a6c350b6ce8560406e6ab4dda528264f
Osmair Coelho <osmair.coelho@gmail.com>
2026-02-17 14:45:11 -0300

    mais 10 registros para ver paginacao

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,12 +17,11 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
 
         $this->call([AdminUserSeeder::class]);
-        Contact::factory()->count(1)->create();
+        Contact::factory()->count(10)->create();
     }
 }
